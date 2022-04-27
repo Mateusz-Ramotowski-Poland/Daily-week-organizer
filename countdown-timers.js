18bf1c1656f047b083d0eb0330aad9ad22ecd8fd
Mateusz-Ramotowski-Poland <83215700+Mateusz-Ramotowski-Poland@users.noreply.github.com>
2022-04-27 10:06:57 +0200

    Createdindex.html, styles.css and js file.In js file I wrote remarks from 2 previous games. In html I start working on skeleton.

---
+++ countdown-timers.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// try not to use global variabes
+//one function - one functionality
+// I can use a fev js script instead of one
+// use Babel to use your code on every browser
+// use get/   verbs in function name
+// use inputs instead of prompt
+// in function name - write what it do not how it do
