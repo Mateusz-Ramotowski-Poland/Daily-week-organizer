755967ddee235afda3204e5d6d3145b34bc3a4db
Mateusz-Ramotowski-Poland <83215700+Mateusz-Ramotowski-Poland@users.noreply.github.com>
2022-05-17 13:56:07 +0200

    I finished refactoring js. file

--- countdown-timers.js
+++ countdown-timers.js
@@ -11,44 +11,39 @@
 // in function name - write what it do not how it do
 ///////////////////////////////////////////////////////////////
 const nodeTimers = document.getElementsByClassName("timer"); // return HTML live collection
-const nodeTimer = document.querySelector(".timer"); // used to add new timer
-const nodeTimersSection = document.querySelector(".timers");
+const nodeTimersSection = document.querySelector(".timers"); // return static NodeList
 const btnAddTimer = document.querySelector(".add-timer");
 const btnDeleteTimer = document.querySelector(".delete-timer");
+const btnPLaySong = document.querySelector(".play-song");
+const btnPauseSong = document.querySelector(".pause-song");
 
 const songAudio = new Audio("piosenka-jest-dobra-na-wszystko.mp3");
 
 const timers = []; // array of intervalID, which is a numeric, non-zero value which identifies the timer created
 ///////////////////////////////////////////////// play songs buttons version - I will delete this section
-document.querySelector(".play-song").addEventListener("click", function () {
-  songAudio.play();
-});
-document.querySelector(".pause-song").addEventListener("click", function () {
-  songAudio.pause();
-  /* songAudio.stop(); // you don't have method stop */
-});
+btnPLaySong.addEventListener("click", songAudio.play.bind(songAudio));
+btnPauseSong.addEventListener("click", songAudio.pause.bind(songAudio));
 ////////////////////////////////////////////////////////////play songs buttons end  I will delete this section
 btnAddTimer.addEventListener("click", function () {
-  if (nodeTimers.length >= 3) return;
+  if (!(nodeTimers.length < 3)) return;
 
-  nodeTimersSection.append(nodeTimer.cloneNode(true));
+  nodeTimersSection.append(nodeTimers[0].cloneNode(true));
 });
 btnDeleteTimer.addEventListener("click", function () {
-  if (nodeTimers.length <= 1) return;
+  if (!(nodeTimers.length > 1)) return;
 
+  deleteTimer(nodeTimers.length - 1);
   nodeTimers.item(nodeTimers.length - 1).remove();
-  deleteTimer();
 });
 function getTimer(event) {
-  for (let i = 0; i < event.currentTarget.children.length; i++) {
-    if (event.currentTarget.children.item(i) === event.target.closest(".timer"))
-      return i; // timerNumber
+  for (const [index, timer] of Object.entries(event.currentTarget.children)) {
+    if (timer === event.target.closest(".timer")) return index;
   }
 }
-function showTime(time, nodeTimer) {
-  const minutes = `${Math.trunc(time / (60 * 1000))}`.padStart(2, "0");
-  const seconds = `${Math.trunc((time / 1000) % 60)}`.padStart(2, "0");
-  nodeTimer.textContent = `${minutes}:${seconds}`;
+function showTime(timerTime, nodeTimerPTag) {
+  const minutes = `${Math.trunc(timerTime / (60 * 1000))}`.padStart(2, "0");
+  const seconds = `${Math.trunc((timerTime / 1000) % 60)}`.padStart(2, "0");
+  nodeTimerPTag.textContent = `${minutes}:${seconds}`;
 }
 function deleteTimer(timerNumber) {
   clearInterval(timers[timerNumber]);
@@ -61,8 +56,8 @@ document.querySelector(".timers").addEventListener("click", function (event) {
     // I put function declaration here to use closure
     return setInterval(function () {
       const passedTime = Date.now() - startTime;
-      showTime(time - passedTime, nodeTimer);
-      if (!(passedTime >= time)) return;
+      showTime(timerSetTime - passedTime, nodeTimerPTag);
+      if (!(passedTime >= timerSetTime)) return;
 
       deleteTimer(timerNumber);
       songAudio.play();
@@ -71,9 +66,9 @@ document.querySelector(".timers").addEventListener("click", function (event) {
 
   const timerNumber = getTimer(event);
   const startTime = Date.now();
-  const nodeTimer = event.target.closest(".timer").querySelector(".timer-time"); // find p tag
-  let time = Math.trunc(20 * timerNumber * 60 * 1000); // time in ms; there was problem with fraction numbers (trunc fixed it)
-  showTime(time, nodeTimer);
+  const nodeTimerPTag = event.target.closest(".timer").querySelector(".timer-time"); 
+  let timerSetTime = Math.trunc(20 * parseInt(timerNumber) * 60 * 1000); // time in ms; there was problem with fraction numbers (trunc fixed it)
+  showTime(timerSetTime, nodeTimerPTag);
   if (timers[timerNumber]) {
     deleteTimer(timerNumber);
   }
