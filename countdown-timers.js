4a95594780a30f6c39b3ef8886ceee9450f5e9c5
Mateusz-Ramotowski-Poland <83215700+Mateusz-Ramotowski-Poland@users.noreply.github.com>
2022-05-17 12:28:38 +0200

    I started code refactoring js. file

--- countdown-timers.js
+++ countdown-timers.js
@@ -11,10 +11,14 @@
 // in function name - write what it do not how it do
 ///////////////////////////////////////////////////////////////
 const nodeTimers = document.getElementsByClassName("timer"); // return HTML live collection
-const nodeTimerClean = document.querySelector(".timer"); // clean timer = without any data inside - it can have data inside
+const nodeTimer = document.querySelector(".timer"); // used to add new timer
 const nodeTimersSection = document.querySelector(".timers");
-const timers = []; // array of intervalID, which is a numeric, non-zero value which identifies the timer created
+const btnAddTimer = document.querySelector(".add-timer");
+const btnDeleteTimer = document.querySelector(".delete-timer");
+
 const songAudio = new Audio("piosenka-jest-dobra-na-wszystko.mp3");
+
+const timers = []; // array of intervalID, which is a numeric, non-zero value which identifies the timer created
 ///////////////////////////////////////////////// play songs buttons version - I will delete this section
 document.querySelector(".play-song").addEventListener("click", function () {
   songAudio.play();
@@ -24,74 +28,63 @@ document.querySelector(".pause-song").addEventListener("click", function () {
   /* songAudio.stop(); // you don't have method stop */
 });
 ////////////////////////////////////////////////////////////play songs buttons end  I will delete this section
-document.querySelector(".add-timer").addEventListener("click", function () {
-  if (nodeTimers.length < 3) {
-    nodeTimersSection.append(nodeTimerClean.cloneNode(true));
-  }
+btnAddTimer.addEventListener("click", function () {
+  if (nodeTimers.length >= 3) return;
+
+  nodeTimersSection.append(nodeTimer.cloneNode(true));
 });
-document.querySelector(".delete-timer").addEventListener("click", function () {
-  if (nodeTimers.length > 1) {
-    nodeTimers.item(nodeTimers.length - 1).remove();
-    // you should also delete timers if they exist
-  }
+btnDeleteTimer.addEventListener("click", function () {
+  if (nodeTimers.length <= 1) return;
+
+  nodeTimers.item(nodeTimers.length - 1).remove();
+  deleteTimer();
 });
 function getTimer(event) {
   for (let i = 0; i < event.currentTarget.children.length; i++) {
-    if (
-      event.currentTarget.children.item(i) === event.target.closest(".timer")
-    ) {
-      const timerNumber = i;
-      return timerNumber;
-    }
+    if (event.currentTarget.children.item(i) === event.target.closest(".timer"))
+      return i; // timerNumber
   }
 }
 function showTime(time, nodeTimer) {
-  const minutes = `${Math.trunc(time / (60*1000))}`.padStart(2, "0");
+  const minutes = `${Math.trunc(time / (60 * 1000))}`.padStart(2, "0");
   const seconds = `${Math.trunc((time / 1000) % 60)}`.padStart(2, "0");
   nodeTimer.textContent = `${minutes}:${seconds}`;
-  console.log(`time:${time},minutes: ${minutes}, seconds:${seconds}`);
 }
 function deleteTimer(timerNumber) {
   clearInterval(timers[timerNumber]);
   timers[timerNumber] = null;
 }
 document.querySelector(".timers").addEventListener("click", function (event) {
-  if (event.target.classList.contains("btnStart")) {
-    //Here I use Event delegation
-    function setTimer() {
-      // I put function declaration here to use closure
-      return setInterval(function () {
-        passedTime = Date.now() - startTime;
-        showTime(time - passedTime, nodeTimer);
-        if (passedTime >= time) {
-          deleteTimer(timerNumber);
-          songAudio.play();
-        }
-      }, 1000);
-    }
+  if (!event.target.classList.contains("btnStart")) return;
+  //Here I use Event delegation
+  function setTimer() {
+    // I put function declaration here to use closure
+    return setInterval(function () {
+      const passedTime = Date.now() - startTime;
+      showTime(time - passedTime, nodeTimer);
+      if (!(passedTime >= time)) return;
 
-    const timerNumber = getTimer(event);
-    console.log(timerNumber);
-    const startTime = Date.now();
-    let passedTime;
-    console.log(startTime);
-    const nodeTimer = event.target
-      .closest(".timer")
-      .querySelector(".timer-time"); // find p tag
-    let time = Math.trunc(20 * timerNumber * 60 * 1000); // time in ms; there was problem with fraction numbers (trunc fixed it)
-    showTime(time, nodeTimer);
-    if (timers[timerNumber]) {
       deleteTimer(timerNumber);
-    }
-    timers[timerNumber] = setTimer();
+      songAudio.play();
+    }, 1000);
   }
+
+  const timerNumber = getTimer(event);
+  const startTime = Date.now();
+  const nodeTimer = event.target.closest(".timer").querySelector(".timer-time"); // find p tag
+  let time = Math.trunc(20 * timerNumber * 60 * 1000); // time in ms; there was problem with fraction numbers (trunc fixed it)
+  showTime(time, nodeTimer);
+  if (timers[timerNumber]) {
+    deleteTimer(timerNumber);
+  }
+  timers[timerNumber] = setTimer();
 });
 document.querySelector(".timers").addEventListener("click", function (event) {
-  if (event.target.classList.contains("btnReset")) {
-    //Here I use Event delegation
-    const timerNumber = getTimer(event);
-    deleteTimer(timerNumber);
-    event.target.closest(".timer").querySelector(".timer-time").textContent =
-      "mm:ss";
-  }
+  if (!event.target.classList.contains("btnReset")) return;
+
+  //Here I use Event delegation
+  const timerNumber = getTimer(event);
+  deleteTimer(timerNumber);
+  event.target.closest(".timer").querySelector(".timer-time").textContent =
+    "mm:ss";
 });
