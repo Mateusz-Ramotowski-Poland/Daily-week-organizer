c2807296b6e1062aee814f7ba6dd0ce222712f2d
Mateusz-Ramotowski-Poland <83215700+Mateusz-Ramotowski-Poland@users.noreply.github.com>
2022-05-18 11:10:22 +0200

    simple to do list - add and delete tasks (without locale storage

--- to-do.js
+++ to-do.js
@@ -1,11 +1,22 @@
-const btnAddTask = document.querySelector('.btn-add-task');
-const btnDeleteTask = document.querySelector('.btn-delete-task');
+const btnAddTask = document.querySelector(".btn-add-task");
+const btnDeleteTask = document.querySelector(".btn-delete-task");
+
+const toDoElement = document.querySelector(".to-do-element");
+const toDoElementsArea = document.querySelector(".to-do-elements");
+
+const toDoDescriptionInput = document.querySelector(
+  'input[id="to-do-new-task-description"]'
+);
 //below only for testing, after development delete these lines
-const addTaskArea = document.querySelector('.add-task-area');         
+const addTaskArea = document.querySelector(".add-task-area");
 ////////////////////////////////////////////////////////////////////
-addTaskArea.addEventListener('click', function(event){
-    console.log(event.target);
-    console.log(!event.target.classList.contains('.btn-add-task'));
-   if (!event.target.classList.contains('btn-add-task')) return;
-    addTaskArea.append(btnAddTask.cloneNode(true));
-})
+btnAddTask.addEventListener("click", function () {
+  const newToDoElement = toDoElement.cloneNode(true);
+  newToDoElement.innerHTML = `${toDoDescriptionInput.value} <button class="btn-delete-task">delete exercise</button>`;
+  toDoElementsArea.append(newToDoElement);
+});
+toDoElementsArea.addEventListener('click', function(event){
+    if(!event.target.classList.contains('btn-delete-task')) return;
+
+    event.target.closest('.to-do-element').remove();
+});
