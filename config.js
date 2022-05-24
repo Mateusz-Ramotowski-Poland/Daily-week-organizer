f37c07341db28eb531ff86bcffbbef7c7f07230b
Mateusz-Ramotowski-Poland <83215700+Mateusz-Ramotowski-Poland@users.noreply.github.com>
2022-05-24 20:57:26 +0200

    Now is better layout for to-do-section and timer section. It is good for now. Now work with form layout

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const minNumberTimers = 1; // it can't be 0; 0 will cause error in countdown-timers.js. 
-export const maxNumberTimers = 3;
+export const maxNumberTimers = 5;
 export const baseTimerTime = '20:00' // time: 20 minutes, 0 seconds
 export const ToDoKeyLocalStorage = 'ToDoTasks';
